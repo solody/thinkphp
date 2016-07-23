7bf412ddbcb0c539f7e4f93b50b221b4427af250
solody <12943125@qq.com>
2016-07-23 22:47:55 +0800

    测试1:1关联更新成功

--- Application/Home/Controller/IndexController.class.php
+++ Application/Home/Controller/IndexController.class.php
@@ -28,6 +28,20 @@ class IndexController extends Controller
 
     public function editUser()
     {
+        $t = function ($unistr, $encoding = 'GBK', $prefix = '&#', $postfix = ';') {
+            $arruni = explode($prefix, $unistr);
+            $unistr = '';
+            for($i = 1, $len = count($arruni); $i < $len; $i++) {
+                if (strlen($postfix) > 0) {
+                    $arruni[$i] = substr($arruni[$i], 0, strlen($arruni[$i]) - strlen($postfix));
+                }
+                $temp = intval($arruni[$i]);
+                $unistr .= ($temp < 256) ? chr(0) . chr($temp) : chr($temp / 256) . chr($temp % 256);
+            }
+            return iconv('UCS-2', $encoding, $unistr);
+        };
+
+
         $getUsername = function (){
             $string = '';
             for ($i=1; $i<8; $i++) {
@@ -36,10 +50,10 @@ class IndexController extends Controller
             return $string;
         };
 
-        $getNickname = function () {
+        $getNickname = function () use ($t) {
             $string = '';
             for ($i=1; $i<8; $i++) {
-                $string .= chr(rand(19968,40869));
+                $string .= $t('&#'.rand(19968,40869).';','utf-8');
             }
             return $string;
         };
@@ -53,6 +67,10 @@ class IndexController extends Controller
             )
         );
 
+        $rs = D('User')->relation(true)->where(array('id'=>1))->save($user_data);
+
         print_r($user_data);
+        echo PHP_EOL;
+        echo $rs;
     }
 }
