ca2a4c740d19246c0779b43de6c3e762bbd2d6ef
solody <12943125@qq.com>
2016-07-23 17:42:55 +0800

    更新数据库

--- Application/Home/Controller/IndexController.class.php
+++ Application/Home/Controller/IndexController.class.php
@@ -25,4 +25,34 @@ class IndexController extends Controller
         $rs = D('User')->relation(true)->add($user_data);
         print_r($rs);
     }
+
+    public function editUser()
+    {
+        $getUsername = function (){
+            $string = '';
+            for ($i=1; $i<8; $i++) {
+                $string .= chr(rand(65,90));
+            }
+            return $string;
+        };
+
+        $getNickname = function () {
+            $string = '';
+            for ($i=1; $i<8; $i++) {
+                $string .= chr(rand(19968,40869));
+            }
+            return $string;
+        };
+
+        $user_data = array(
+            'username'=>$getUsername(),
+            'password'=>'123456',
+            'person'=>array(
+                'nickname'=>$getNickname(),
+                'avatar'=>'^o^'
+            )
+        );
+
+        print_r($user_data);
+    }
 }
